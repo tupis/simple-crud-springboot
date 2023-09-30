340259feb9e6a39af5af808b50989afb3c0af554
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-09-30 16:59:43 -0300

    fix: name of route

--- src/main/java/com/tupi/controllers/PersonController.java
+++ src/main/java/com/tupi/controllers/PersonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Person")
+@RequestMapping("/person")
 public class PersonController {
 
     @Autowired
