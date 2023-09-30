883b537e1e5bf3c43fb89b854a437bf17d76c1d7
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-09-30 16:59:58 -0300

    fix: correct entity in repo

--- src/main/java/com/tupi/repositories/PersonRepository.java
+++ src/main/java/com/tupi/repositories/PersonRepository.java
@@ -1,8 +1,8 @@
 package com.tupi.repositories;
 
-import com.tupi.data.vo.v1.PersonVO;
+import com.tupi.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends JpaRepository<PersonVO, Long> {}
+public interface PersonRepository extends JpaRepository<Person, Long> {}
