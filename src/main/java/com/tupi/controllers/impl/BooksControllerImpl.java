a969fc9fe3fc1f97a0aa10f0c087829da22036aa
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-10-13 00:01:00 -0300

    feat: add documentation swagger

--- src/main/java/com/tupi/controllers/impl/BooksControllerImpl.java
+++ src/main/java/com/tupi/controllers/impl/BooksControllerImpl.java
@@ -2,9 +2,15 @@ package com.tupi.controllers.impl;
 
 import com.tupi.controllers.BooksController;
 import com.tupi.data.vo.v1.BooksVO;
+import com.tupi.data.vo.v1.PersonVO;
 import com.tupi.services.impl.BooksServiceImpl;
 import com.tupi.utils.LogUtil;
 import com.tupi.utils.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +28,55 @@ public class BooksControllerImpl implements BooksController {
 
     @Override
     @GetMapping("/findAll")
+    @Operation(
+            summary = "find all books",
+            description = "find all books",
+            tags = {"Books"},
+            responses = {
+                    @ApiResponse(
+                            description = "success",
+                            responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON,
+                                            array = @ArraySchema(schema = @Schema(implementation = BooksVO.class))
+                                    )
+                            }
+                    ),
+                    @ApiResponse(description = "No Content", responseCode = "2044", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
     public List<BooksVO> findAll() {
         return service.findAll();
     }
 
     @Override
     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(
+            summary = "find one book",
+            description = "find one book",
+            tags = {"Books"},
+            responses = {
+                    @ApiResponse(
+                            description = "success",
+                            responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON,
+                                            schema = @Schema(implementation = BooksVO.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
     public BooksVO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
@@ -38,6 +87,26 @@ public class BooksControllerImpl implements BooksController {
             produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
             consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
     )
+    @Operation(
+            summary = "Create one book",
+            description = "Create one book",
+            tags = {"Books"},
+            responses = {
+                    @ApiResponse(
+                            description = "success",
+                            responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON,
+                                            schema = @Schema(implementation = BooksVO.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
     public BooksVO create(@RequestBody BooksVO books) {
         return service.create(books);
     }
@@ -47,14 +116,47 @@ public class BooksControllerImpl implements BooksController {
             produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
             consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
     )
+    @Operation(
+            summary = "Update one book",
+            description = "Update one book",
+            tags = {"Books"},
+            responses = {
+                    @ApiResponse(
+                            description = "Update",
+                            responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON,
+                                            schema = @Schema(implementation = BooksVO.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
     public BooksVO update(Long id, BooksVO books) {
-        return null;
+        return service.update(id, books);
     }
 
     @Override
     @DeleteMapping(
             value = "/{id}",
             produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}
+    )
+    @Operation(
+            summary = "Delete one book",
+            description = "Delete one book",
+            tags = {"Books"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
     )
     public ResponseEntity<String> deleteById(@PathVariable("id") Long id) {
         return service.deleteById(id);
