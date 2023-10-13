ec89667698dabbbeccc0b0c34492ab4a3009046a
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-10-12 23:32:41 -0300

    fix: change path version

--- src/main/java/com/tupi/controllers/PersonController.java
+++ src/main/java/com/tupi/controllers/PersonController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/person/v1")
+@RequestMapping("/api/v1/person")
 @Tag(name = "People", description = "endpoing for managing people")
 public class PersonController {
 
