ff3606d31ae0baa576c54ff71ecdb609778e9c66
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-10-21 19:02:59 -0300

    chore: CORS enable globally

--- src/main/java/com/tupi/config/WebConfig.java
+++ src/main/java/com/tupi/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.tupi.config;
 
 import com.tupi.serialization.converter.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,6 +14,21 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+    @Value("${cors.origin.patterns:http://localhost:8080}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowCredentials(true)
+//                .allowedMethods("*")
+                .maxAge(3600);
+
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
