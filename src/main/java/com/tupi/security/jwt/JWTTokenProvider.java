60467d01b3efbb9f719845233edd564c8f5012fc
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-11-05 23:34:27 -0300

    feat: create provider jwt

---
+++ src/main/java/com/tupi/security/jwt/JWTTokenProvider.java
@@ -0,0 +1,107 @@
+package com.tupi.security.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.tupi.data.vo.v1.security.TokenVO;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Base64;
+import java.util.Date;
+
+@Service
+public class JWTTokenProvider {
+
+    @Value("${security.jwt.token.secret:secret}")
+    private String secretKey = "secret";
+
+    @Value("${security.jwt.token.expire-length:3600000}")
+    private Long validityInMilliseconds = 3600000L;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    Algorithm algorithm = null;
+    Integer validityRefreshToken = 3;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        algorithm = Algorithm.HMAC256(secretKey.getBytes());
+    }
+
+    public TokenVO createAccessToken(String username, String[] roles) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+        var accessToken = getAccessToken(username, roles, now, validity);
+        var refreshToken = getRefreshToken(username, roles, now);
+
+        return new TokenVO(username,true, now, validity, accessToken, refreshToken);
+    }
+    private String getAccessToken(String username, String[] roles, Date now, Date validity) {
+        String issuerURL = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+        return JWT.create()
+                .withArrayClaim("roles", roles)
+                .withIssuedAt(now)
+                .withExpiresAt(validity)
+                .withSubject(username)
+                .withIssuer(issuerURL)
+                .sign(algorithm)
+                .strip();
+    }
+
+    private String getRefreshToken(String username, String[] roles, Date now) {
+        Date validityRefreshToken = new Date(now.getTime() + (this.validityInMilliseconds * this.validityRefreshToken));
+        return JWT.create()
+                .withArrayClaim("roles", roles)
+                .withIssuedAt(now)
+                .withExpiresAt(validityRefreshToken)
+                .withSubject(username)
+                .sign(algorithm)
+                .strip();
+    }
+
+
+    public Authentication getAuthentication(String token) {
+        DecodedJWT decoded = decodedToken(token);
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(decoded.getSubject());
+
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private DecodedJWT decodedToken(String token) {
+        Algorithm alg = Algorithm.HMAC256(secretKey.getBytes());
+        JWTVerifier verifier = JWT.require(alg).build();
+        return verifier.verify(token);
+    }
+
+    public String resolveToken(HttpServletRequest req) {
+        String bearerToken = req.getHeader("Authorization");
+
+        if(
+                bearerToken == null &&
+                !bearerToken.startsWith("Bearer ") &&
+                bearerToken.split("Bearer")[1] == null
+        ) {
+            return null;
+        }
+
+        return bearerToken.split("Bearer")[1];
+    }
+
+    public Boolean validateToken(String token) {
+        DecodedJWT decoded = decodedToken(token);
+        return !decoded.getExpiresAt().before(new Date());
+    }
+
+}
