9e055efca408fe8307a98429a8e33ad9da2735b3
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-12-09 23:10:10 -0300

    feat: configure SecurityConfig

--- src/main/java/com/tupi/config/SecurityConfig.java
+++ src/main/java/com/tupi/config/SecurityConfig.java
@@ -2,56 +2,60 @@ package com.tupi.config;
 
 import com.tupi.security.jwt.JWTConfigurer;
 import com.tupi.security.jwt.JWTTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
 @Configuration
-@Profile({"dev", "test"})
+@EnableWebSecurity
+//@Profile({"dev", "test"})
 public class SecurityConfig {
 
-        private final JWTTokenProvider provider;
+    private final String [] WHITE_LIST = {
+            "/swagger-ui/**",
+            "/api-docs/**",
+            "/auth/signIn",
+    };
 
-        public SecurityConfig(JWTTokenProvider provider) {
-            this.provider = provider;
-        }
+    @Autowired
+    private JWTTokenProvider provider;
 
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
+    public SecurityConfig(JWTTokenProvider provider) {
+        this.provider = provider;
+    }
 
-        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-            return authenticationConfiguration.getAuthenticationManager();
-        }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authHttpReq -> authHttpReq
-                        .requestMatchers(
-                                "/auth/signin",
-                                "/auth/refresh/**",
-                                "/swagger-ui/**",
-                                "/v3/api-docs"
-                        ).permitAll()
+                        .requestMatchers(WHITE_LIST).permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .requestMatchers("/users").denyAll()
                 )
-                .cors(cors -> {
-                })
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .apply(new JWTConfigurer(provider));
+
         return http.build();
     }
 
