4598f982d0438ba16346bda1b321a3685f35ed10
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-12-09 23:07:06 -0300

    fix: use query without @Param

--- src/main/java/com/tupi/repositories/UserRepository.java
+++ src/main/java/com/tupi/repositories/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User u WHERE u.userName =: userName")
-    User findUsersByUserName(@Param("userName") String userName);
+    @Query("SELECT u FROM User u WHERE u.userName = ?1")
+    User findUsersByUserName(String userName);
 }
